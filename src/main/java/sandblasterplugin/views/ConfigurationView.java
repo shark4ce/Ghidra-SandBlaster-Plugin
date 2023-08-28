74ecaecefb37723dde3b703fb6ef961843c6c904
shark4ce <sirbusandu98@gmail.com>
2023-08-28 18:51:32 +0300

    clean up

--- src/main/java/sandblasterplugin/views/ConfigurationView.java
+++ src/main/java/sandblasterplugin/views/ConfigurationView.java
@@ -5,7 +5,6 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.awt.Insets;
 import java.io.InputStream;
 import java.util.Scanner;
 
